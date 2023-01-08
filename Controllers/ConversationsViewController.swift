625a41f2eb21d0d6f53e5fbcf29b9d467a027e96
WilsonMungai <wilsonmuguthi@gmail.com>
2023-01-08 18:02:53 -0500

    getting started

---
+++ Controllers/ConversationsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  iOS-Messenger-App
+//
+//  Created by Wilson Mungai on 2023-01-08.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
